796ae37e911cc9f8f7ff18ab78fa677f5a822fc1
Maurits Ouwerkerk <Maurits.ouwerkerk@gmail.com>
2023-01-23 20:31:11 +0100

    more cleanup

--- injector/src/string.h
+++ injector/src/string.h
@@ -3,9 +3,14 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-void (*printlen)(char *, int);
-size_t (*strlen)(char*);
-void (*printcrlf)(void);
-int (*sprintf)(char*, char*, ...);
+#define PRINTBUF 0x40e895e9
+#define PRINTCRLF 0x40e88a49
+#define STRLEN 0x40f146a5
+#define SPRINTF 0x40f0c891
+
+void (*printlen)(char *, int) = (void*) PRINTBUF;
+size_t (*strlen)(char*) = (void*) STRLEN;
+void (*printcrlf)(void) = (void*) PRINTCRLF;
+int (*sprintf)(char*, char*, ...) = (void*) SPRINTF;
 
 #endif
